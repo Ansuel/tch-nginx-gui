a370249e37705824a3417e6b6f5521f6e61e69fa
Ansuel Smith <ansuelsmth@gmail.com>
2018-03-08 17:00:46 +0100

    Update 5.3.11
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

---
+++ usr/share/transformer/scripts/refresh_driver.sh
@@ -0,0 +1,12 @@
+sleep 7
+
+installed_driver=$(transformer-cli get rpc.xdsl.dslversion | awk '{print $4}')
+driver_set=$(uci get env.var.driver_version)
+
+if [ $installed_driver != $driver_set ]; then
+	logger "Downloading driver "$driver_set
+	wget https://repository.ilpuntotecnicoeadsl.com/files/Ansuel/AGTEF/adsl_driver/$driver_set -O /etc/adsl/adsl_phy.bin
+	logger "Restarting xdslctl"
+	xdslctl stop
+	/etc/init.d/xdsl start
+fi
