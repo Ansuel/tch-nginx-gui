0ac96572bf90c841c68b0d49335165b28d546723
Ansuel Smith <ansuelsmth@gmail.com>
2018-04-07 13:17:40 +0200

    Update 6.13.14
    Various fixes
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- usr/share/transformer/scripts/refresh_driver.sh
+++ usr/share/transformer/scripts/refresh_driver.sh
@@ -5,7 +5,8 @@ driver_set=$(uci get env.var.driver_version)
 
 if [ $installed_driver != $driver_set ]; then
 	logger "Downloading driver "$driver_set
-	wget https://repository.ilpuntotecnicoeadsl.com/files/Ansuel/AGTEF/adsl_driver/$driver_set -O /etc/adsl/adsl_phy.bin
+	wget https://repository.ilpuntotecnicoeadsl.com/files/Ansuel/AGTEF/adsl_driver/$driver_set -O /tmp/
+	mv /tmp/$driver_set /etc/adsl/adsl_phy.bin
 	logger "Restarting xdslctl"
 	xdslctl stop
 	/etc/init.d/xdsl start
