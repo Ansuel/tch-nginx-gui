5ddc9654fb613fcfc45725cfba8073885ef010dd
SeproDE <SeproDE@users.noreply.github.com>
2018-04-16 00:55:13 +0200

    First changes for mobile support
    Under 50rem the sidebar gets hidden

--- www/docroot/theme/Fritz/script.js
+++ www/docroot/theme/Fritz/script.js
@@ -1,10 +1,19 @@
 var elmArr = document.getElementsByClassName("content");
 
+
+var styleEl = document.createElement('style'), styleSheet;
+document.head.appendChild(styleEl);
+styleSheet = styleEl.sheet;
+
+styleSheet.insertRule('.content { transform: translateY( 0px);}');
+var ruleR1 = styleSheet.cssRules[0];
+styleSheet.insertRule('.row[style="z-index : 2;  position: relative;"] { width: 15.5rem;}');
+var ruleR2 = styleSheet.cssRules[0];
+styleSheet.insertRule('.apprise-overlay { opacity: 0 !important}');
+var ruleR3 = styleSheet.cssRules[0];
+
 window.addEventListener('scroll', function(){
-	var i;
-	for (i = 0; i < elmArr.length; i++) { 
-		elmArr[i].style.transform = "translateY( -" + window.scrollY + "px)";
-	}
+	ruleR1.style.transform = 'translateY( -' + window.scrollY + 'px)';
 });
 
 var updateHeight = (function(){
@@ -16,8 +25,36 @@ var updateHeight = (function(){
 			maxheight = height;
 		} 
 	}
-	document.getElementsByTagName("body")[0].style.height = (maxheight+30) + "px";
+	document.body.style.height = (maxheight+30) + "px";
 });
 
 window.onresize = updateHeight;
-window.onload = updateHeight;
+window.onload = function(){
+  updateHeight;
+  closeNav();
+  document.querySelector('.header.span12').addEventListener('click', function (e) {
+    if (e.pageX  < 60) {
+        openNav();
+    }
+  });
+  
+  document.querySelector('.apprise-overlay').style.opacity = "";
+  document.querySelector('.apprise-overlay').addEventListener('click', function (e) {
+    closeNav();
+  });
+  document.querySelector('.row[style="z-index : 2;  position: relative;"]').addEventListener('click', function (e) {
+    closeNav();
+  });
+}
+
+function openNav() {
+	ruleR2.style.width  = "15.5rem";
+	ruleR3.style.opacity = "0.4";
+	ruleR3.style.pointerEvents  = "all";
+}
+
+function closeNav() {
+	ruleR2.style.width  = "0";
+	ruleR3.style.opacity = "0";
+	ruleR3.style.pointerEvents  = "none";
+}
