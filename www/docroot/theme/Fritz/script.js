48935212a54b514dc53eba5f6257a29461f28bb7
SeproDE <SeproDE@users.noreply.github.com>
2018-04-18 13:57:53 +0200

    More optimization for mobile usage

--- www/docroot/theme/Fritz/script.js
+++ www/docroot/theme/Fritz/script.js
@@ -5,18 +5,44 @@ var styleEl = document.createElement('style'), styleSheet;
 document.head.appendChild(styleEl);
 styleSheet = styleEl.sheet;
 
-styleSheet.insertRule('.content { transform: translateY( 0px);}');
+styleSheet.insertRule('.row .smallcard .content { transform: translateY( 0px);}');
 var ruleR1 = styleSheet.cssRules[0];
-styleSheet.insertRule('.row[style="z-index : 2;  position: relative;"] { width: 15.5rem;}');
+styleSheet.insertRule('.row[style="z-index : 2;  position: relative;"] { marginLeft: 0;}');
 var ruleR2 = styleSheet.cssRules[0];
-styleSheet.insertRule('.apprise-overlay { opacity: 0 !important}');
+styleSheet.insertRule('.apprise-overlay { opacity: 0 !important; pointer-events: none}');
 var ruleR3 = styleSheet.cssRules[0];
+styleSheet.insertRule('.row[style="z-index : 3;  position: relative;"] { transform: translateY( 0px);}');
+var ruleR4 = styleSheet.cssRules[0];
 
 window.addEventListener('scroll', function(){
 	ruleR1.style.transform = 'translateY( -' + window.scrollY + 'px)';
 });
 
-var updateHeight = (function(){
+window.onload = function(){
+  
+  document.querySelector('.apprise-overlay').style.opacity = "";
+  
+  if (document.querySelector('.row[style="z-index : 2;  position: relative;"]') != null){
+    window.onresize = updateHeight;
+    updateHeight();
+    closeNav();
+    document.querySelector('.header.span12').addEventListener('click', function (e) {
+      if (e.pageX  < 60) {
+        openNav();
+      }
+    });
+    document.querySelector('.apprise-overlay').addEventListener('click', function (e) {
+      closeNav();
+    });
+    document.querySelector('.row[style="z-index : 2;  position: relative;"]').addEventListener('click', function (e) {
+	  if (e.target.childNodes.length == 2)
+        e.target.childNodes[0].click();
+	  closeNav();
+    });
+  }
+}
+
+function updateHeight(){
 	var maxheight = 0;
 	var j;
 	for (j = 0; j < elmArr.length; j++) { 
@@ -26,35 +52,16 @@ var updateHeight = (function(){
 		} 
 	}
 	document.body.style.height = (maxheight+30) + "px";
-});
-
-window.onresize = updateHeight;
-window.onload = function(){
-  updateHeight;
-  closeNav();
-  document.querySelector('.header.span12').addEventListener('click', function (e) {
-    if (e.pageX  < 60) {
-        openNav();
-    }
-  });
-  
-  document.querySelector('.apprise-overlay').style.opacity = "";
-  document.querySelector('.apprise-overlay').addEventListener('click', function (e) {
-    closeNav();
-  });
-  document.querySelector('.row[style="z-index : 2;  position: relative;"]').addEventListener('click', function (e) {
-    closeNav();
-  });
 }
 
 function openNav() {
-	ruleR2.style.width  = "15.5rem";
-	ruleR3.style.opacity = "0.4";
+	ruleR2.style.marginLeft  = "0";
+	ruleR3.style.opacity = "0.6";
 	ruleR3.style.pointerEvents  = "all";
 }
 
 function closeNav() {
-	ruleR2.style.width  = "0";
+	ruleR2.style.marginLeft  = "-15.5rem";
 	ruleR3.style.opacity = "0";
 	ruleR3.style.pointerEvents  = "none";
 }
