56b39313f2570fa6ba00dce9b2bf47bb3199d58b
Ansuel Smith <ansuelsmth@gmail.com>
2018-05-19 19:10:49 +0200

    Add license to project
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- decompressed/gui_file/usr/share/transformer/scripts/resetgui.sh
+++ decompressed/gui_file/usr/share/transformer/scripts/resetgui.sh
@@ -1,3 +1,27 @@
+#
+#
+#	 Custom Gui for Technicolor Modem: utility script and modified gui for the Technicolor Modem
+#	 								   interface based on OpenWrt
+#
+#    Copyright (C) 2018  Christian Marangi <ansuelsmth@gmail.com>
+#
+#    This file is part of Custom Gui for Technicolor Modem.
+#    
+#    Custom Gui for Technicolor Modem is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#    
+#    Custom Gui for Technicolor Modem is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#    
+#    You should have received a copy of the GNU General Public License
+#    along with Custom Gui for Technicolor Modem.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
 rm -r /www/*
 rm /etc/nginx/nginx.conf
 rm -r /usr/share/transformer/*
