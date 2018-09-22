4994798bde5d14f35f0277382e02f49edbf2c782
Ansuel Smith <ansuelsmth@gmail.com>
2018-09-22 19:45:44 +0200

    Update 8.8.12
    Include latest pull request
    Finished support for telstra gui
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- decompressed/gui_file/usr/share/transformer/scripts/install_telstra.sh
+++ decompressed/gui_file/usr/share/transformer/scripts/install_telstra.sh
@@ -1,3 +1,4 @@
 curl -k https://raw.githubusercontent.com/Ansuel/tch-nginx-gui/master/modular/telstra_gui.tar.bz2 --output /tmp/telstra_gui.tar.bz2
 bzcat /tmp/telstra_gui.tar.bz2 | tar -C / -xf - 
 rm /tmp/telstra_gui.tar.bz2
+/etc/init.d/nginx restart
--- decompressed/gui_file/usr/share/transformer/scripts/remove_telstra.sh
+++ decompressed/gui_file/usr/share/transformer/scripts/remove_telstra.sh
@@ -6,4 +6,5 @@ if [ -d /www/telstra-snippets ]; then
 	rm -r /www/docroot/telstra-helpfiles/
 	rm /www/docroot/js/main-telstra-min.js
 	rm /www/docroot/css/gw-telstra.css/gw-telstra.css
+	/etc/init.d/nginx restart
 fi
