81f83af1dedc4920c533c0367fc337e4f49fb680
FrancYescO <francyesc0@hotmail.it>
2018-09-28 01:22:42 +0200

    add images to telstra gui

--- decompressed/gui_file/usr/share/transformer/scripts/remove_telstra.sh
+++ decompressed/gui_file/usr/share/transformer/scripts/remove_telstra.sh
@@ -3,7 +3,8 @@ if [ -d /www/telstra-snippets ]; then
 	rm /www/gateway-snippets/telstra-gui.lp
 	rm /www/docroot/telstra-gui.lp
 	rm -r /www/docroot/telstra-modals
-	rm -r /www/docroot/telstra-helpfiles/
+	rm -r /www/docroot/telstra-helpfiles
+	rm -r /www/docroot/img/telstra
 	rm /www/docroot/js/main-telstra-min.js
 	rm /www/docroot/css/gw-telstra.css/gw-telstra.css
 	/etc/init.d/nginx restart
