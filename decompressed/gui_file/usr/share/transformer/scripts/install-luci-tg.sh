02b5cdd9705a861cbfaece04b7cd00cdb0d8a25a
Kevdagoat <kevdagoat@gmail.com>
2018-09-28 17:58:10 +0800

    Fix url

--- decompressed/gui_file/usr/share/transformer/scripts/install-luci-tg.sh
+++ decompressed/gui_file/usr/share/transformer/scripts/install-luci-tg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-curl -k -L https://github.com/nutterpc/tg-luci/blob/master/install.sh --output /tmp/install.sh
+curl -k -L https://raw.githubusercontent.com/nutterpc/tg-luci/master/install.sh --output /tmp/install.sh
 chmod +x /tmp/install.sh
 /tmp/install.sh
