ef8326badd6dde3b20c64ee2bd0a8747831b68a4
Kevdagoat <kevdagoat@gmail.com>
2018-09-28 17:57:11 +0800

    Update install-luci-tg.sh

--- decompressed/gui_file/usr/share/transformer/scripts/install-luci-tg.sh
+++ decompressed/gui_file/usr/share/transformer/scripts/install-luci-tg.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-filename="nutterpc-tg-luci-6f3cf6e"
-
 curl -k -L https://github.com/nutterpc/tg-luci/blob/master/install.sh --output /tmp/install.sh
 chmod +x /tmp/install.sh
 /tmp/install.sh
-
-
-
