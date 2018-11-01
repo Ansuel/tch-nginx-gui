05e7ede140249aca9821bee9175f05ee9021e6ca
Ansuel Smith <ansuelsmth@gmail.com>
2018-11-02 00:25:27 +0100

    Update 8.9.47
    Fix transmission install for DGA modem
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- decompressed/gui_file/usr/share/transformer/scripts/remove_transmission.sh
+++ decompressed/gui_file/usr/share/transformer/scripts/remove_transmission.sh
@@ -1,4 +1,4 @@
-opkg remove --force-removal-of-dependent-packages transmission-daemon
+opkg remove --force-removal-of-dependent-packages transmission-daemon-openssl transmission-web
 rm -r /www/docroot/transmission
 rm -r /etc/config/transmission*
 rm -r /var/transmission
