26fb84b027e42d294730aa090408073a3bc44f8d
Ansuel Smith <ansuelsmth@gmail.com>
2018-11-13 23:23:41 +0100

    Fix wrong driver name and move download link to github
    Currently the driver name include also the kernel module version of the xdsl system. Remove this and move download link to github.
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- decompressed/gui_file/usr/share/transformer/scripts/refresh_driver.sh
+++ decompressed/gui_file/usr/share/transformer/scripts/refresh_driver.sh
@@ -37,22 +37,22 @@ try=0
 sleep 7
 
 checksums="
-453dc537d3cbd6f45657c9eb8ba2eb40  A2pvbH042j2.d26r
-9086cc14d865b8ec090345545425ba40  A2pvbH042n.d26r
-7bcf3f18e4b25e1fdf16412e5832930c  A2pvbH042o.d26r
-db76ca2f5015dd17504f1c658373e0aa  A2pvbH042r.d26r
-bcfaed886a6dfd6cd2a0be2ac4e3823c  A2pvbH042u.d26r
-a981a610dc01a932216a3508cf3e325f  A2pvfbH043b.d26r
-fbaade8f05cad074403b693fedc9a2b8  A2pvfbH043f.d26r
-fbaade8f05cad074403b693fedc9a2b8  A2pvfbH043g.d26r
-5988d3c56b0fee3ce0c3843cda4358b1  A2pvfbH043k.d26r
-6e8de6c77db38a5b5394f7f7c4221196  A2pvfbH043o.d26r
-efee94080f1ab7e29a1c7a571a6c8488  B2pvbH042l.d26r
-2a44b0dd6c0b24c29f8ea1aab10d60f0  B2pvbH042s.d26r
-3f85469797162bda08fabc28ed325042  B2pvfbH043e.d26r
-42e93153db06857021faf1de1b320f6d  B2pvfbH043g.d26r
-d76d9eebfccd572b0a0943cd3957b82a  B2pvfbH043i1.d26r
-1767c3c86b5e4f5b46ce024ccd4f91b2  B2pvfbH043k.d26r
+453dc537d3cbd6f45657c9eb8ba2eb40  A2pvbH042j2
+9086cc14d865b8ec090345545425ba40  A2pvbH042n
+7bcf3f18e4b25e1fdf16412e5832930c  A2pvbH042o
+db76ca2f5015dd17504f1c658373e0aa  A2pvbH042r
+bcfaed886a6dfd6cd2a0be2ac4e3823c  A2pvbH042u
+a981a610dc01a932216a3508cf3e325f  A2pvfbH043b
+fbaade8f05cad074403b693fedc9a2b8  A2pvfbH043f
+fbaade8f05cad074403b693fedc9a2b8  A2pvfbH043g
+5988d3c56b0fee3ce0c3843cda4358b1  A2pvfbH043k
+6e8de6c77db38a5b5394f7f7c4221196  A2pvfbH043o
+efee94080f1ab7e29a1c7a571a6c8488  B2pvbH042l
+2a44b0dd6c0b24c29f8ea1aab10d60f0  B2pvbH042s
+3f85469797162bda08fabc28ed325042  B2pvfbH043e
+42e93153db06857021faf1de1b320f6d  B2pvfbH043g
+d76d9eebfccd572b0a0943cd3957b82a  B2pvfbH043i1
+1767c3c86b5e4f5b46ce024ccd4f91b2  B2pvfbH043k
 "
 
 installed_driver=$(transformer-cli get rpc.xdsl.dslversion | awk '{print $4}')
@@ -67,7 +67,7 @@ apply_driver() {
 
 download_Driver() {
 	logger "Downloading driver "$driver_set
-	remote_driver_dir=https://repository.ilpuntotecnico.com/files/Ansuel/AGTEF/adsl_driver
+	remote_driver_dir=https://github.com/Ansuel/tch-nginx-gui/raw/dev/modular/xdsl_driver/arm/
 	$curl $remote_driver_dir/$driver_set --output /tmp/$driver_set
 }
 
