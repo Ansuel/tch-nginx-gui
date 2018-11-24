2b0986492c15b0c0f54e6ce0edd36a4673abce32
Ansuel Smith <ansuelsmth@gmail.com>
2018-11-24 19:36:28 +0100

    make install telstra script use the right branch
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- decompressed/gui_file/usr/share/transformer/scripts/install_telstra.sh
+++ decompressed/gui_file/usr/share/transformer/scripts/install_telstra.sh
@@ -1,4 +1,10 @@
-curl -k https://raw.githubusercontent.com/Ansuel/tch-nginx-gui/master/modular/telstra_gui.tar.bz2 --output /tmp/telstra_gui.tar.bz2
+if [ $(uci get env.var.update_branch) == "dev" ]; then
+	branch="dev"
+else
+	branch="master"
+fi
+
+curl -k https://raw.githubusercontent.com/Ansuel/tch-nginx-gui/$branch/modular/telstra_gui.tar.bz2 --output /tmp/telstra_gui.tar.bz2
 bzcat /tmp/telstra_gui.tar.bz2 | tar -C / -xf - 
 rm /tmp/telstra_gui.tar.bz2
 /etc/init.d/nginx restart
