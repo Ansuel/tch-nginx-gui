c4ec5d104d9f7db4fd0535918ac66fa357b75c83
Francesco Molitierno <franc.molitierno@gmail.com>
2018-12-11 00:50:36 +0100

    fix telstra install URL

--- decompressed/gui_file/usr/share/transformer/scripts/install_telstra.sh
+++ decompressed/gui_file/usr/share/transformer/scripts/install_telstra.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-if [ $(uci get env.var.update_branch) == "dev" ]; then
-	branch="dev"
-else
-	branch="master"
-fi
-
-curl -k https://raw.githubusercontent.com/Ansuel/tch-nginx-gui/$branch/modular/telstra_gui.tar.bz2 --output /tmp/telstra_gui.tar.bz2
+curl -k https://raw.githubusercontent.com/Ansuel/gui-dev-build-auto/master/modular/telstra_gui.tar.bz2 --output /tmp/telstra_gui.tar.bz2
 bzcat /tmp/telstra_gui.tar.bz2 | tar -C / -xf - 
 rm /tmp/telstra_gui.tar.bz2
 /etc/init.d/nginx restart
