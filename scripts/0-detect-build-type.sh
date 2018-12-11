ffeb555bfb8ae1452db6de6a211d7aa29d4aa555
Ansuel Smith <ansuelsmth@gmail.com>
2018-12-11 21:08:23 +0100

    Fix circleci bug and ledfw never handle notok situation
    This fix a bug in circleci and a bug in status-led-eventing that return always ok even if service are NOT ok
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- scripts/0-detect-build-type.sh
+++ scripts/0-detect-build-type.sh
@@ -1,6 +1,6 @@
 last_log="$(git log --oneline -n 1)"
 
-if [ "$( echo "$last_log" | grep "[STABLE]" )" ]; then
+if [ "$( echo "$last_log" | grep "\[STABLE\]" )" ]; then
 	touch ~/.stable
 else
 	touch ~/.dev
