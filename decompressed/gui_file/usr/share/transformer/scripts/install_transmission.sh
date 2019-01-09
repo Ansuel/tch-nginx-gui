666d325e1fdce055e069a97d66dd2e8e26fd21b6
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-09 01:53:32 +0100

    partially implement https://github.com/Ansuel/tch-nginx-gui/issues/415

--- decompressed/gui_file/usr/share/transformer/scripts/install_transmission.sh
+++ decompressed/gui_file/usr/share/transformer/scripts/install_transmission.sh
@@ -1,13 +1,33 @@
 #!/bin/sh
-opkg update
-opkg install transmission-web transmission-daemon-openssl
 
-uci set transmission.@transmission[0].enabled=1
-uci set transmission.@transmission[0].rpc_whitelist='127.0.0.1,192.168.*'
-uci commit
+install_DGA() {
+    opkg update
+    opkg install transmission-web transmission-daemon-openssl
 
-cp -r /usr/share/transmission /www/docroot/
-rm /www/docroot/transmission/web/index.html /www/docroot/transmission/web/LICENSE
+    uci set transmission.@transmission[0].enabled=1
+    uci set transmission.@transmission[0].rpc_whitelist='127.0.0.1,192.168.*'
+    uci commit
 
-/etc/init.d/transmission enable
-/etc/init.d/transmission restart
+    cp -r /usr/share/transmission /www/docroot/
+    rm /www/docroot/transmission/web/index.html /www/docroot/transmission/web/LICENSE
+
+    /etc/init.d/transmission enable
+    /etc/init.d/transmission restart
+}
+
+install_from_github(){
+    curl -sLk https://github.com/$1/tarball/$2 --output /tmp/$2.tar.gz
+    mkdir /tmp/$2
+    tar -xzf /tmp/$2.tar.gz -C /tmp/$2
+    rm /tmp/$2.tar.gz
+    cd /tmp/$2/*
+    chmod +x ./setup.sh
+	./setup.sh
+	rm -r /tmp/$2
+}
+
+device_type="$(uci get -q env.var.prod_friendly_name)"
+
+[ "$(echo $device_type | grep DGA)" ] && install_DGA
+
+[ "$(echo $device_type | grep TG789)" ] && install_from_github FrancYescO/sharing_tg789 transmission
