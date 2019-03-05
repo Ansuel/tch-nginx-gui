6b89601eb02b707086255e0fca74906916795859
Ansuel Smith <ansuelsmth@gmail.com>
2019-03-05 18:17:40 +0100

    webui: try to improve error 503
    When no session are available try to clean session with no activity past 20 seconds
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- decompressed/gui_file/usr/lib/lua/web/sessionmgr.lua
+++ decompressed/gui_file/usr/lib/lua/web/sessionmgr.lua
@@ -334,7 +334,23 @@ end
 local function redirectIfServiceNotAvailable(session)
   if not session then
 	ngx.log(ngx.ERR, "Service not available")
-    ngx.exit(ngx.HTTP_SERVICE_UNAVAILABLE)
+    ngx.exit(ngx.HTTP_SERVICE_UNAVAILABLE, "Service not available. Probably too much session open. Wait 20 seconds and retry.")
+  end
+end
+
+
+local function cleanupIfNoSession(self , session)
+
+  if not session then
+	
+	--Remove every session inactive for 20 second
+	for sid, session in pairs(self.sessions) do
+		local expired = sessionExpired(self, session, sid, now, timeout)
+		if expired then
+			ngx.log(ngx.ERR,"Removed session "..sid)
+		end
+	end
+	
   end
 end
 
@@ -361,6 +377,7 @@ end
 --     for the session won't be updated.
 function SessionMgr:checkrequest(noActivityUpdate)
   local session, sessionID = getSessionForRequest(self, noActivityUpdate)
+  cleanupIfNoSession(self,session)
   redirectIfServiceNotAvailable(session)
   preventDNSRebind()
   redirectIfNotAuthorized(self, session, sessionID)
@@ -482,6 +499,7 @@ function SessionMgr:handleAuth()
   if uri ~= self.authpath and uri ~= self.passpath then
     return
   end
+
   -- only POST is allowed
   if ngx.req.get_method() ~= "POST" then
     ngx.exit(ngx.HTTP_FORBIDDEN)  -- doesn't return
