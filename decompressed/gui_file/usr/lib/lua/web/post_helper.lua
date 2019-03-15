bdebc0accbf79a6552ed9e7ce5f72b37171f76e9
Ansuel Smith <ansuelsmth@gmail.com>
2019-03-15 20:13:00 +0100

    webui: fix bug with time translation
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- decompressed/gui_file/usr/lib/lua/web/post_helper.lua
+++ decompressed/gui_file/usr/lib/lua/web/post_helper.lua
@@ -192,6 +192,7 @@ end
 -- @param #number time
 -- @return #string number of seconds
 function M.secondsToTime(time)
+  setlanguage()
   local time_no = tonumber(time)
   if (time_no and time_no >= 0) then
     local durations = {
@@ -227,6 +228,7 @@ end
 -- @param #number time
 -- @return #string number of seconds
 function M.secondsToTimeShort(time)
+  setlanguage()
   local time_no = tonumber(time)
   if (time_no and time_no >= 0) then
     local durations = {
--- decompressed/gui_file/www/docroot/ajax/cpuload.lua
+++ decompressed/gui_file/www/docroot/ajax/cpuload.lua
@@ -1,19 +1,16 @@
--- Enable localization
-gettext.textdomain('webui-mobiled')
-
 local json = require("dkjson")
 local proxy = require("datamodel")
-local content_helper = require("web.content_helper")
+local readfile = require("web.content_helper").readfile
 local post_helper = require("web.post_helper")
 local ngx = ngx
 
 local data = {
 	cpuusage = proxy.get("sys.proc.CPUUsage")[1].value .. "%" or "0",
 	ram_free = math.floor(tonumber(proxy.get("sys.mem.RAMFree")[1].value) / 1024) or "0",
-	uptime = post_helper.secondsToTime(content_helper.readfile("/proc/uptime","number",floor)),
-	connection = content_helper.readfile("/proc/sys/net/netfilter/nf_conntrack_count"),
+	uptime = post_helper.secondsToTime(readfile("/proc/uptime","number",floor)),
+	connection = readfile("/proc/sys/net/netfilter/nf_conntrack_count"),
 	system_time = os.date("%F %T", os.time()),
-	cpuload = content_helper.readfile("/proc/loadavg","string"):sub(1,14),
+	cpuload = readfile("/proc/loadavg","string"):sub(1,14),
 }
 
 local buffer = {}
