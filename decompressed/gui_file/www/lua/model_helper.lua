490cc9de201e85daff7be6f5976c7e1a9d9c88ef
Ansuel Smith <ansuelsmth@gmail.com>
2019-05-15 23:07:47 +0200

    web: move model to separate lua for easy implementation
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

---
+++ decompressed/gui_file/www/lua/model_helper.lua
@@ -0,0 +1,10 @@
+return {
+ "DGA4130" = "AGTEF",
+ "DGA4132" = "AGTHP",
+ "MediaAccess DGA4131FWB" = "DGA4131FWB",
+ "Technicolor TG799vac" = "TG799vac",
+ "Technicolor TG800vac" = "TG800vac",
+ "Technicolor TG789vac V2" = "TG789vac",
+ "Technicolor TG788vn" = "TG788vn",
+ "MediaAccess TG788vn v2" = "TG788vn"
+}
