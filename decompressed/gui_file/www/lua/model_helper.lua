14a2c9be952bed6fbda4b2de1ca79921442c4af0
Ansuel Smith <ansuelsmth@gmail.com>
2019-05-21 21:17:42 +0200

    web: adds Telstra Smartmodem image
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- decompressed/gui_file/www/lua/model_helper.lua
+++ decompressed/gui_file/www/lua/model_helper.lua
@@ -7,6 +7,7 @@ return {
  ["Technicolor TG789vac V2"] = "TG789vac",
  ["MediaAccess TG789vac v2"] = "TG789vac",
  ["Technicolor TG788vn"] = "TG788vn",
- ["MediaAccess TG788vn v2"] = "TG788vn"
+ ["MediaAccess TG788vn v2"] = "TG788vn",
+ ["Technicolor DJA0230TLS"] = "DJA0230",
 }
 
