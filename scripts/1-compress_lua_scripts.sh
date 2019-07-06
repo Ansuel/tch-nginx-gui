a270c7850060e6060c28645d57b04069a4fd4924
Ansuel Smith <ansuelsmth@gmail.com>
2019-07-06 15:28:00 +0200

    circleci: fix some error in compress lua script
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- scripts/1-compress_lua_scripts.sh
+++ scripts/1-compress_lua_scripts.sh
@@ -14,16 +14,15 @@ minify_lua() {
 	fi
 	echo "Minying $file | Pretranslated:"$append_pretraslate
 	luasrcdiet --maximum --quiet $1 -o $1.min
-	if [[ $? != 0 ]]; then echo "Minify error for $1"; fi
+	if [[ $? != 0 ]]; then echo "Minify error for $1";return; fi
 	if [ $append_pretraslate == 1 ]; then
 		sed -i '1s/^/'"$pretranslated_string"'\n/' $1.min
-		perl -i -pe 's|(\ *\t*)\/\/(.*)\\\n?|$1\/\*$2 *\/\\\n|g' $1.min
-		sed -i ':a;N;$!ba;s/\\\n\s*\t*//g' $1.min
-		chmod $(stat -c "%a" $1) $1.min
-		compressed=$(($(stat --printf="%s" $1)-$(stat --printf="%s" $1.min)))
-		echo "File $1 minified for $compressed byte"
-		saved=$((saved + compressed))
 	fi
+	perl -i -pe 's|(\ *\t*)\/\/(.*)\\\n?|$1\/\*$2 *\/\\\n|g' $1.min
+	sed -i ':a;N;$!ba;s/\\\n\s*\t*//g' $1.min
+	chmod $(stat -c "%a" $1) $1.min
+	compressed=$(($(stat --printf="%s" $1)-$(stat --printf="%s" $1.min)))
+	echo "File $1 minified for $compressed byte"
 }
 
 for file in `find . -name "*.lua" -type f`; do
@@ -47,6 +46,3 @@ for file in `find . -name "*.lua" -o -name "*.lp" -o -name "*.map" -type f`; do
 		mv $file.min $file
 	fi
 done
-
-echo "By compressing lua scripts we saved $saved byte"
-echo "Finished"
