e02e80b8837e98a2af4d9c4b031bb35f4ef51e3f
Ansuel Smith <ansuelsmth@gmail.com>
2019-07-10 23:11:05 +0200

    web: improve card limiter
    Adds way to exclude some specific path from card limiter
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- decompressed/gui_file/www/lua/cards_limiter.lua
+++ decompressed/gui_file/www/lua/cards_limiter.lua
@@ -2,36 +2,55 @@
 local M = {}
 
 local bridge_limit_list = {
-  ["gateway.lp"] = true,
-  ["modgui.lp"] = true,
-  ["broadband.lp"] = true,
-  ["internet.lp"] = true,
-  ["wireless.lp"] = true,
-  ["LAN.lp"] = true,
-  ["usermgr.lp"] = true,
-  ["diagnostics.lp"] = true,
-  ["iproutes.lp"] = true,
-  ["system.lp"] = true,
-  ["xdsl.lp"] = true,
-  ["extensions.lp"] = true,
-  ["eco.lp"] = true,  
+	["/www/cards/"] = {
+		["gateway.lp"] = true,
+		["modgui.lp"] = true,
+		["broadband.lp"] = true,
+		["internet.lp"] = true,
+		["wireless.lp"] = true,
+		["LAN.lp"] = true,
+		["usermgr.lp"] = true,
+		["diagnostics.lp"] = true,
+		["iproutes.lp"] = true,
+		["system.lp"] = true,
+		["xdsl.lp"] = true,
+		["extensions.lp"] = true,
+		["eco.lp"] = true,
+	},
+	["/www/info-cards/"] = {
+		["gateway.lp"] = true,
+		["wanstats.lp"] = true,
+		["graph.lp"] = true,
+		["xdsl_stats.lp"] = true,
+		["internet.lp"] = true,
+	}
 }
 
 local voice_limit_list = {
-  ["gateway.lp"] = true,
-  ["modgui.lp"] = true,
-  ["broadband.lp"] = true,
-  ["internet.lp"] = true,
-  ["wireless.lp"] = true,
-  ["LAN.lp"] = true,
-  ["usermgr.lp"] = true,
-  ["telephony.lp"] = true,
-  ["diagnostics.lp"] = true,
-  ["iproutes.lp"] = true,
-  ["system.lp"] = true,
-  ["xdsl.lp"] = true,
-  ["extensions.lp"] = true,
-  ["eco.lp"] = true,
+	["/www/cards/"] = {
+		["gateway.lp"] = true,
+		["modgui.lp"] = true,
+		["broadband.lp"] = true,
+		["internet.lp"] = true,
+		["wireless.lp"] = true,
+		["LAN.lp"] = true,
+		["usermgr.lp"] = true,
+		["telephony.lp"] = true,
+		["diagnostics.lp"] = true,
+		["iproutes.lp"] = true,
+		["system.lp"] = true,
+		["xdsl.lp"] = true,
+		["extensions.lp"] = true,
+		["eco.lp"] = true,
+	},
+	["/www/info-cards/"] = {
+		["gateway.lp"] = true,
+		["wanstats.lp"] = true,
+		["graph.lp"] = true,
+		["xdsl_stats.lp"] = true,
+		["mmpbxd_stats.lp"] = true,
+		["internet.lp"] = true,
+	}
 }
 
 function M.get_limit_info()
@@ -45,12 +64,15 @@ function M.get_limit_info()
 	return {bridgemode=bridgemode_status,voicemode=voicemode_status}
 end
 
-function M.card_limited(info, cardname)
+function M.card_limited(info, cardname, includepath)
+  if not includepath:match("/$") then 
+	includepath = includepath .. "/"
+  end
   if info.bridgemode or info.voicemode then
 	if info.bridgemode then
-		return not bridge_limit_list[cardname]
+		return not bridge_limit_list[includepath][cardname]
 	else
-		return not voice_limit_list[cardname]
+		return not voice_limit_list[includepath][cardname]
 	end
   end
   return false
