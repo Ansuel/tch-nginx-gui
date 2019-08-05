341f80ab67ebc3d4c800015ded7a4dc73b7811a8
Francesco Molitierno <franc.molitierno@gmail.com>
2019-08-05 22:39:31 +0200

    modgui scripts: shellcheck script improve specific flag set webui: move specific message to extension modal

--- decompressed/gui_file/etc/modgui_scripts/02_specific.sh
+++ decompressed/gui_file/etc/modgui_scripts/02_specific.sh
@@ -1,113 +1,109 @@
 . /etc/init.d/rootdevice
 
 extract_with_check() {
-	
-	export RESTART_SERVICE=0
-	MD5_CHECK_DIR=/tmp/md5check
-	
-	[ ! -d $MD5_CHECK_DIR ] && mkdir $MD5_CHECK_DIR
-	
-	for file in $(bzcat $1 | tar -C $MD5_CHECK_DIR -xvf -); do
-	
-		if [ ! -f $MD5_CHECK_DIR/$file ]; then
-			if [ ! -d /$file ]; then
-				mkdir /$file
-			fi
-			continue
-		fi
-		
-		[ -n "$( echo $file | grep .md5sum )" ] && continue
-		
-		orig_file=/$file
-		file=$MD5_CHECK_DIR/$file
-		
-		if [ -f $orig_file ]; then
-			md5_file=$(md5sum $file | awk '{ print $1 }' )
-			md5_orig_file=$(md5sum $orig_file | awk '{ print $1 }' )
-			if [ $md5_file == $md5_orig_file ]; then
-				rm $file
-				continue
-			fi
-		fi
-		
-		cp $file $orig_file
-		rm $file
-		RESTART_SERVICE=1
-		
-	done
-	
-	[ -d $MD5_CHECK_DIR ] && rm -r $MD5_CHECK_DIR
-	
-	rm $1
-	
-	return $RESTART_SERVICE
+
+  export RESTART_SERVICE=0
+  MD5_CHECK_DIR=/tmp/md5check
+
+  [ ! -d $MD5_CHECK_DIR ] && mkdir $MD5_CHECK_DIR
+
+  for file in $(bzcat "$1" | tar -C $MD5_CHECK_DIR -xvf -); do
+
+    if [ ! -f "$MD5_CHECK_DIR/$file" ]; then
+      if [ ! -d "/$file" ]; then
+        mkdir "/$file"
+      fi
+      continue
+    fi
+
+    [ -n "$(echo "$file" | grep .md5sum)" ] && continue
+
+    orig_file=/$file
+    file=$MD5_CHECK_DIR/$file
+
+    if [ -f "$orig_file" ]; then
+      md5_file=$(md5sum "$file" | awk '{ print $1 }')
+      md5_orig_file=$(md5sum "$orig_file" | awk '{ print $1 }')
+      if [ "$md5_file" == "$md5_orig_file" ]; then
+        rm "$file"
+        continue
+      fi
+    fi
+
+    cp "$file" "$orig_file"
+    rm "$file"
+    RESTART_SERVICE=1
+
+  done
+
+  [ -d $MD5_CHECK_DIR ] && rm -r $MD5_CHECK_DIR
+
+  rm $1
+
+  return $RESTART_SERVICE
 }
 
 ledfw_extract() {
-	if [ -f "/tmp/ledfw_support-specific$1.tar.bz2" ]; then
-		extract_with_check "/tmp/ledfw_support-specific$1.tar.bz2"
-		[ $? -eq 1 ] && /usr/share/transformer/scripts/restart_leds.sh
-	fi
+  if [ -f "/tmp/ledfw_support-specific$1.tar.bz2" ]; then
+    extract_with_check "/tmp/ledfw_support-specific$1.tar.bz2"
+    [ $? -eq 1 ] && /usr/share/transformer/scripts/restart_leds.sh
+  fi
 }
 
 ledfw_rework_TG788() {
-	if [ ! "$(uci get -q button.info)" ] || [ "$(uci get -q button.info)" == "BTN_3" ]; then
-		logger_command "Setting up status (wifi) button..."
-		uci del button.easy_reset
-		uci set button.info=button
-		uci set button.info.button='BTN_1'
-		uci set button.info.action='released'
-		uci set button.info.handler='logger INFO button pressed ; ubus send infobutton '\''{"state":"active"}'\'''
-		uci set button.info.min='0'
-		uci set button.info.max='2'
-		uci set button.eco.min='2'
-		uci set button.eco.max='5'
-		uci set button.acl.min='5'
-		uci set ledfw.iptv.check='0'
-		uci commit ledfw
-	fi
+  if [ ! "$(uci get -q button.info)" ] || [ "$(uci get -q button.info)" == "BTN_3" ]; then
+    logger_command "Setting up status (wifi) button..."
+    uci del button.easy_reset
+    uci set button.info=button
+    uci set button.info.button='BTN_1'
+    uci set button.info.action='released'
+    uci set button.info.handler='logger INFO button pressed ; ubus send infobutton '\''{"state":"active"}'\'''
+    uci set button.info.min='0'
+    uci set button.info.max='2'
+    uci set button.eco.min='2'
+    uci set button.eco.max='5'
+    uci set button.acl.min='5'
+    uci set ledfw.iptv.check='0'
+    uci commit ledfw
+  fi
 
-    ledfw_extract "TG788"
+  ledfw_extract "TG788"
 }
 
 ledfw_rework_TG799() {
-	if [ ! "$(uci get -q button.info)" ]; then
-		logger_command "Setting up status (power) button..."
-		uci del button.easy_reset
-		uci set button.info=button
-		uci set button.info.button='BTN_3'
-		uci set button.info.action='released'
-		uci set button.info.handler='logger INFO button pressed ; ubus send infobutton '\''{"state":"active"}'\'''
-		uci set button.info.min='0'
-		uci set button.info.max='2'
-		uci set ledfw.iptv.check='0'
-		uci commit ledfw
-	fi
+  if [ ! "$(uci get -q button.info)" ]; then
+    logger_command "Setting up status (power) button..."
+    uci del button.easy_reset
+    uci set button.info=button
+    uci set button.info.button='BTN_3'
+    uci set button.info.action='released'
+    uci set button.info.handler='logger INFO button pressed ; ubus send infobutton '\''{"state":"active"}'\'''
+    uci set button.info.min='0'
+    uci set button.info.max='2'
+    uci set ledfw.iptv.check='0'
+    uci commit ledfw
+  fi
 
-	ledfw_extract "TG799"
+  ledfw_extract "TG799"
 }
 
 ledfw_rework_TG800() {
-	if [ ! "$(uci get -q button.info)" ]; then
-		logger_command "Setting up status (wifi) button..."
-		uci del button.easy_reset
-		uci set button.info=button
-		uci set button.info.button='BTN_1'
-		uci set button.info.action='released'
-		uci set button.info.handler='logger INFO button pressed ; ubus send infobutton '\''{"state":"active"}'\'''
-		uci set button.info.min='0'
-		uci set button.info.max='2'
-		uci set button.wifi_on_off_toggle.min='2'
-		uci set button.wifi_on_off_toggle.max='8'
-		uci set ledfw.iptv.check='0'
-		uci commit ledfw
-	fi
+  if [ ! "$(uci get -q button.info)" ]; then
+    logger_command "Setting up status (wifi) button..."
+    uci del button.easy_reset
+    uci set button.info=button
+    uci set button.info.button='BTN_1'
+    uci set button.info.action='released'
+    uci set button.info.handler='logger INFO button pressed ; ubus send infobutton '\''{"state":"active"}'\'''
+    uci set button.info.min='0'
+    uci set button.info.max='2'
+    uci set button.wifi_on_off_toggle.min='2'
+    uci set button.wifi_on_off_toggle.max='8'
+    uci set ledfw.iptv.check='0'
+    uci commit ledfw
+  fi
 
-    ledfw_extract "TG800"
-}
-
-clean_specific_file() {
-	rm /tmp/*specific*.tar.bz2
+  ledfw_extract "TG800"
 }
 
 #wifi_fix_24g() {
@@ -120,46 +116,57 @@ clean_specific_file() {
 #}
 
 remove_downgrade_bit() {
-	if [ "$(uci get -q env.rip.board_mnemonic)" == "VBNT-S" ] && 
-		[ "$(uci get -q env.var.prod_number)" == "4132" ] && 
-		[ -f /proc/rip/0123 ]; then
-		logger_command "Downgrade limitation bit detected... Removing..."
-		rmmod keymanager
-		rmmod ripdrv
-		mv /lib/modules/3.4.11/ripdrv.ko /lib/modules/3.4.11/ripdrv.ko_back
-		mv /tmp/ripdrv.ko /lib/modules/3.4.11/ripdrv.ko
-		insmod ripdrv
-		echo 0123 > /proc/rip/delete
-		echo 0122 > /proc/rip/delete
-		rmmod ripdrv
-		logger_command "Restoring original driver"
-		rm /lib/modules/3.4.11/ripdrv.ko
-		mv /lib/modules/3.4.11/ripdrv.ko_back /lib/modules/3.4.11/ripdrv.ko
-		insmod ripdrv
-		insmod keymanager
-	elif [ -f /tmp/ripdrv.ko ]; then
-		rm /tmp/ripdrv.ko
-	fi
+  logger_command "Checking downgrade limitation bit"
+  if [ "$(uci get -q env.rip.board_mnemonic)" == "VBNT-S" ] &&
+    [ "$(uci get -q env.var.prod_number)" == "4132" ] &&
+    [ -f /proc/rip/0123 ]; then
+    logger_command "Downgrade limitation bit detected... Removing..."
+    rmmod keymanager
+    rmmod ripdrv
+    mv /lib/modules/3.4.11/ripdrv.ko /lib/modules/3.4.11/ripdrv.ko_back
+    mv /tmp/ripdrv.ko /lib/modules/3.4.11/ripdrv.ko
+    insmod ripdrv
+    echo 0123 >/proc/rip/delete
+    echo 0122 >/proc/rip/delete
+    rmmod ripdrv
+    logger_command "Restoring original driver"
+    rm /lib/modules/3.4.11/ripdrv.ko
+    mv /lib/modules/3.4.11/ripdrv.ko_back /lib/modules/3.4.11/ripdrv.ko
+    insmod ripdrv
+    insmod keymanager
+  elif [ -f /tmp/ripdrv.ko ]; then
+    rm /tmp/ripdrv.ko
+  fi
 }
 
 install_specific() {
-	/usr/share/transformer/scripts/appInstallRemoveUtility.sh install specific_app $1
-	uci set modgui.app.specific_app="1"
+  if ping -q -c 1 -W 1 8.8.8.8 >/dev/null 2>&1; then
+    logger_command "Applying specific model fixes..."
+    uci set modgui.app.specific_app="0"
+    /usr/share/transformer/scripts/appInstallRemoveUtility.sh install specific_app $1
+    uci set modgui.app.specific_app="1"
+  else
+    logger_command "No connection detected, install specific upgrade pack manually!"
+    uci set modgui.app.specific_app="0"
+  fi
 }
 
 #THIS CHECK DEVICE TYPE AND INSTALL SPECIFIC FILE
 device_type="$(uci get -q env.var.prod_friendly_name)"
-kernel_ver="$(cat /proc/version | awk '{print $3}')"
+kernel_ver="$(< /proc/version awk '{print $3}')"
 
-uci set modgui.app.specific_app="0"
-if ping -q -c 1 -W 1 8.8.8.8 >/dev/null 2>&1; then
-	logger_command "Applying specific model fixes..."
-	[ -z "${device_type##*DGA413*}" ] && install_specific DGA
-	[ -z "${kernel_ver##3.4*}" ] && [ -z "${device_type##*TG789*}" ] && install_specific TG789
-	[ -z "${kernel_ver##3.4*}" ] && [ -z "${device_type##*TG799*}" ] && install_specific TG789
-	[ -z "${kernel_ver##3.4*}" ] && [ -z "${device_type##*TG800*}" ] && install_specific TG800
-else
-	logger_command "No connection detected, install specific upgrade pack manually!"
+if [ ! "$(uci get -q modgui.app.specific_app)" ]; then
+  uci set modgui.app.specific_app="0"
+fi
+
+if [ -z "${device_type##*DGA413*}" ]; then
+  install_specific DGA
+elif [ -z "${kernel_ver##3.4*}" ] && [ -z "${device_type##*TG789*}" ]; then
+  install_specific TG789
+elif [ -z "${kernel_ver##3.4*}" ] && [ -z "${device_type##*TG799*}" ]; then
+  install_specific TG789
+elif [ -z "${kernel_ver##3.4*}" ] && [ -z "${device_type##*TG800*}" ]; then
+  install_specific TG800
 fi
 
 uci commit modgui
@@ -174,30 +181,29 @@ uci commit modgui
 #[ -z "${device_type##*DGA413*}" ] && wifi_fix_24g
 
 #Use custom driver to remove this... thx @Roleo
-[ -z "${kernel_ver##3.4*}" ] && [ -z "${device_type##*DGA413*}" ] && logger_command "Checking downgrade limitation bit"
-[ -z "${kernel_ver##3.4*}" ] && [ -z "${device_type##*DGA413*}" ] && remove_downgrade_bit 
+[ -z "${kernel_ver##3.4*}" ] && [ -z "${device_type##*DGA413*}" ] && remove_downgrade_bit
 
-	#Fix led issues
-	if [ -z "${device_type##*DGA4131*}" ] ; then
-        if [ ! "$(uci get -q ledfw.ambient.active)" ] ; then
-            uci set ledfw.ambient=led
-            uci set ledfw.ambient.active='1'
-            uci commit ledfw
-        fi
-	else
-        if [ ! "$(uci get -q ledfw.status_led.enable)" ] ; then
-            uci set ledfw.status_led=status_led
-            uci set ledfw.status_led.enable='0'
-            uci commit ledfw
-        fi
-        if [ ! "$(uci get -q ledfw.wifi.nsc_on)" ] ; then
-            uci set ledfw.wifi=service
-            uci set ledfw.wifi.nsc_on='1'
-            uci commit ledfw
-        fi
-	fi
-	
+#Fix led issues
+if [ -z "${device_type##*DGA4131*}" ]; then
+  if [ ! "$(uci get -q ledfw.ambient.active)" ]; then
+    uci set ledfw.ambient=led
+    uci set ledfw.ambient.active='1'
+    uci commit ledfw
+  fi
+else
+  if [ ! "$(uci get -q ledfw.status_led.enable)" ]; then
+    uci set ledfw.status_led=status_led
+    uci set ledfw.status_led.enable='0'
+    uci commit ledfw
+  fi
+  if [ ! "$(uci get -q ledfw.wifi.nsc_on)" ]; then
+    uci set ledfw.wifi=service
+    uci set ledfw.wifi.nsc_on='1'
+    uci commit ledfw
+  fi
+fi
+
 if [ -f /tmp/custom-ripdrv-specificDGA.tar.bz2 ]; then
-	clean_specific_file
-	logger_command "Removing fixes and resuming root process..."
+  logger_command "Removing ripdrv and resuming root process..."
+  rm /tmp/*specific*.tar.bz2
 fi
