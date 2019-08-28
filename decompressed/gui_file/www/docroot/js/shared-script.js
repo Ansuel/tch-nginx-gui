95ff529736207c8a7c636d2577bcb5794e5f00e3
Francesco Molitierno <franc.molitierno@gmail.com>
2019-08-28 23:42:58 +0200

    webui: fix update, show script execution status while installing app, minor refactoring

--- decompressed/gui_file/www/docroot/js/shared-script.js
+++ decompressed/gui_file/www/docroot/js/shared-script.js
@@ -3,7 +3,7 @@ var connectionissue = 0;
 
 var modgui = modgui || {};
 !function (module) {
-	
+
 	function postAction(action,logModal) {
 		var target = $(".modal form").attr("action");
 		$.post(
@@ -17,21 +17,21 @@ var modgui = modgui || {};
 		logModal && tch.openModal("/modals/command-log-read-modal.lp");
 		return false;
 	}
-	
+
 	var intervalAjaxScript;
 	var scriptRequestTimeout = 1000;
-	
+
 	function scriptRequestStatusAjax(type) {
 		if (intervalAjaxScript) return;
 		if (type == "checkver") post("checkver");
 		intervalAjaxScript = setInterval(function () {
 			$.ajax({
-					url: "modals/modgui-modal.lp",
-					data: "action=scriptRequestStatus" + "&auto_update=true",
-					timeout: scriptRequestTimeout,
-					cache: false,
-					dataType: "json"
-				})
+				url: "modals/modgui-modal.lp",
+				data: "action=scriptRequestStatus" + "&auto_update=true",
+				timeout: scriptRequestTimeout,
+				cache: false,
+				dataType: "json"
+			})
 				.done(function (data) {
 					if (data.state == "Complete") {
 						if ((type == "checkver") && (data.new_version_text)) {
@@ -61,12 +61,12 @@ var modgui = modgui || {};
 				});
 		}, scriptRequestTimeout);
 	}
-	
+
 	function createAjaxUpdateCard(CardIdRefresh, ajaxLink, IntervalVar, RefreshTime, CustomRefreshFunction) {
-		
+
 		var element = document.getElementById(CardIdRefresh);
 		if (!element) return ;
-		
+
 		var ElementBinding = {};
 		var ElementBindingList = [];
 		var ObserveElement;
@@ -75,9 +75,9 @@ var modgui = modgui || {};
 			ElementBindingList.push(ObserveElement);
 			ElementBinding[ObserveElement] = ko.observable();
 		});
-	
+
 		var arrayLength = ElementBindingList.length;
-	
+
 		var AjaxRefresh = ( typeof CustomRefreshFunction === "function" ) && CustomRefreshFunction || function() {
 			var updateLink = "auto_update=true";
 			if ( /[a-z]+=[a-z]+/.test(ajaxLink) ) {
@@ -110,14 +110,14 @@ var modgui = modgui || {};
 						tch.showProgress(connectionLost + " " + data.statusText);
 				});
 		};
-		
+
 		AjaxRefresh(ElementBinding);
-		
+
 		if (!ko.dataFor(element))
 			ko.applyBindings(ElementBinding, element);
 		KoRequest[IntervalVar] = setInterval(AjaxRefresh,RefreshTime,ElementBinding);
 	}
-	
+
 	function linkCheckUpdate() {
 		$(".check_update").on("click", function (e) {
 			e.stopPropagation();
@@ -131,39 +131,39 @@ var modgui = modgui || {};
 					cache: false,
 					dataType: "json"
 				})
-				.done(function (data) {
-					if (data.state == "Complete") {
-						if (data.new_version_text) {
-							var outdated_ver = gui_var.gui_outdated;
-							var no_new = gui_var.gui_updated;
-							$(".gui_version_status_text").parent().fadeOut().fadeIn();
-							if (data.new_version_text == "Unknown") {
-								$(".gui_version_status").removeClass("yellow");
-								$(".gui_version_status").addClass("green");
-								$(".gui_version_status_text").text(no_new);
-								$("#upgrade-alert").addClass("hide");
-							} else {
-								$(".gui_version_status").removeClass("green");
-								$(".gui_version_status").addClass("yellow");
-								$("#upgradebtn").removeClass("hide");
-								$(".gui_version_status_text").text(outdated_ver);
-								$("#upgrade-alert").removeClass("hide");
-								$("#new-version-text").text(data.new_version_text);
+					.done(function (data) {
+						if (data.state == "Complete") {
+							if (data.new_version_text) {
+								var outdated_ver = gui_var.gui_outdated;
+								var no_new = gui_var.gui_updated;
+								$(".gui_version_status_text").parent().fadeOut().fadeIn();
+								if (data.new_version_text == "Unknown") {
+									$(".gui_version_status").removeClass("yellow");
+									$(".gui_version_status").addClass("green");
+									$(".gui_version_status_text").text(no_new);
+									$("#upgrade-alert").addClass("hide");
+								} else {
+									$(".gui_version_status").removeClass("green");
+									$(".gui_version_status").addClass("yellow");
+									$("#upgradebtn").removeClass("hide");
+									$(".gui_version_status_text").text(outdated_ver);
+									$("#upgrade-alert").removeClass("hide");
+									$("#new-version-text").text(data.new_version_text);
+								}
+								$(".check_update_spinner").removeClass("fa-spin");
+								clearInterval(KoRequest.CheckVer);
+								KoRequest.CheckVer = null;
 							}
-							$(".check_update_spinner").removeClass("fa-spin");
-							clearInterval(KoRequest.CheckVer);
-							KoRequest.CheckVer = null;
 						}
-					}
-				});
+					});
 			}, scriptRequestTimeout);
 		})
 	};
-	
+
 	function freshStyle(stylesheet) {
 		$("#theme_skin").attr("href", "/theme/" + stylesheet);
 	}
-	
+
 	function scrollFunction() {
 		if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
 			$("#scroll-up").removeClass("hide");
@@ -173,13 +173,13 @@ var modgui = modgui || {};
 			$("#scroll-down").removeClass("hide");
 		}
 	}
-	
+
 	module.postAction = postAction,
-	module.scriptRequestStatusAjax = scriptRequestStatusAjax,
-	module.createAjaxUpdateCard = createAjaxUpdateCard,
-	module.linkCheckUpdate = linkCheckUpdate,
-	module.freshStyle = freshStyle,
-	module.scrollFunction = scrollFunction
+		module.scriptRequestStatusAjax = scriptRequestStatusAjax,
+		module.createAjaxUpdateCard = createAjaxUpdateCard,
+		module.linkCheckUpdate = linkCheckUpdate,
+		module.freshStyle = freshStyle,
+		module.scrollFunction = scrollFunction
 }
 (modgui);
 
@@ -205,11 +205,11 @@ $(function () {
 			$(":root").get(0).style.setProperty("--first-color-accent-80", "rgba(" + colorR + "," + colorG + "," + colorB + ", 0.8)");
 		}, 750);
 	}
-	
+
 	var pathname = document.location.pathname;
 	var page = gui_var.pageselector_page;
 	var text = gui_var.pageselector_text;
-	
+
 	if (pathname == "/stats.lp") {
 		$("#cards-text").text(gui_var.cards_text);
 		document.title = "Gateway - "+gui_var.stats_text;
@@ -219,12 +219,12 @@ $(function () {
 	} else if (pathname == "/" ) {
 		document.title = "Gateway - "+gui_var.pageselector_othertext;
 	}
-	
-	$("#swtichbuttom").on("click", function () {
+
+	$("#switchViewButton").on("click", function () {
 		var pathname = document.location.pathname;
 		var text = gui_var.pageselector_othertext;
 		var view = gui_var.pageselector_text;
-		
+
 		if (pathname == "/stats.lp") {
 			page = "cards.lp";
 			text = gui_var.stats_text;
@@ -234,7 +234,7 @@ $(function () {
 			text = gui_var.cards_text;
 			view = gui_var.stats_text;
 		}
-		
+
 		$("#cards-text").text(openMsg);
 		$("#refresh-cards").show();
 		$("#refresh-cards").css("margin-right", "5px");
@@ -250,7 +250,7 @@ $(function () {
 			$("#refresh-cards").hide();
 			window.history.pushState("gateway", "Gateway - "+view, page);
 			document.title = "Gateway - "+view;
-			$("#swtichbuttom").trigger("switchcard");
+			$("#switchViewButton").trigger("switchcard");
 		});
 	});
 	$("#upgradebtn").on("hover",
@@ -261,11 +261,11 @@ $(function () {
 			$("#upgradebtn").css("color", "orangered");
 		}
 	);
-	
+
 	if ((gui_var.autoupgradeview != "") && (gui_var.autoupgradeview != "none")) {
 		modgui.postAction("autoupgrade_view");
 	};
-	
+
 	if ( gui_var.gui_animation == "1" ) {
 		AOS.init();
 	};
@@ -295,16 +295,8 @@ $(document).ready(function () {
 		},
 		update: function (element, valueAccessor) {
 			var value = ko.unwrap(valueAccessor());
-			if (value != $(element).text()) {
-				if (!$(element).hasClass("hide") && gui_var.gui_animation == "1") {
-					$(element).fadeOut(function () {
-						$(this).text(value).fadeIn();
-					});
-				} else {
-					$(element).text(value);
-				}
-				$(element).parent().parent().parent().animate({scrollTop: $(element).parent().parent().parent()[0].scrollHeight}, 600);
-			}
+			$(element).text(value);
+			$(element).parent().parent().parent().animate({scrollTop: $(element).parent().parent().parent()[0].scrollHeight}, 600);
 		}
 	};
 	ko.bindingHandlers.html = {
