1d153c88c2e020a7e596ca59fac3407e2eee0e0e
Francesco Molitierno <franc.molitierno@gmail.com>
2020-04-08 02:44:12 +0200

    fix xdsl drivers download

--- decompressed/gui_file/usr/share/transformer/scripts/refresh_driver.sh
+++ decompressed/gui_file/usr/share/transformer/scripts/refresh_driver.sh
@@ -6,17 +6,17 @@
 #    Copyright (C) 2018  Christian Marangi <ansuelsmth@gmail.com>
 #
 #    This file is part of Custom Gui for Technicolor Modem.
-#    
+#
 #    Custom Gui for Technicolor Modem is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
-#    
+#
 #    Custom Gui for Technicolor Modem is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
-#    
+#
 #    You should have received a copy of the GNU General Public License
 #    along with Custom Gui for Technicolor Modem.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -125,8 +125,7 @@ apply_driver() {
 
 download_Driver() {
 	log "Downloading driver $driver_set"
-	remote_driver_dir=https://raw.githubusercontent.com/Ansuel/tch-nginx-gui/master/xdsl_driver/$arch/
-	$curl "$remote_driver_dir/$driver_set" --output "/tmp/$driver_set"
+	curl -s "https://raw.githubusercontent.com/Ansuel/tch-nginx-gui/master/xdsl_driver/$arch/$driver_set" --output "/tmp/$driver_set"
 }
 
 test_apply() {
