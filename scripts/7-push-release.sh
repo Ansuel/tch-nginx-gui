6ce9c6dd958ebe71b4356a807841d224ea4ee126
Ansuel Smith <ansuelsmth@gmail.com>
2020-04-12 20:25:51 +0200

    ci: support custom branch build version
    [ci skip]
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- scripts/7-push-release.sh
+++ scripts/7-push-release.sh
@@ -1,3 +1,4 @@
+#!/sh/bash
 
 DIR="$HOME/gui_build/compressed"
 VERSION="$(cat $HOME/gui_build/data/version)"
@@ -6,4 +7,6 @@ if [ $TYPE == "DEV" ] || [ $TYPE == "PREVIEW" ]; then
 	PRERELEASE="-prerelease"
 fi
 
-ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} $PRERELEASE -delete "$VERSION" "$DIR"
+if [ $TYPE == "PREVIEW" ] || [ $TYPE == "STABLE" ] || [ $TYPE == "DEV" ]; then
+	ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} $PRERELEASE -delete "$VERSION" "$DIR"
+fi
