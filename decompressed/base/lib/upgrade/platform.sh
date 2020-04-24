ea0c1b7496f308733c09c0eeb1138f4915a54467
Ansuel Smith <ansuelsmth@gmail.com>
2020-04-24 03:12:07 +0200

    fix broken sysupgrade script
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- decompressed/base/lib/upgrade/platform.sh
+++ decompressed/base/lib/upgrade/platform.sh
@@ -5,6 +5,16 @@
 overlay_dir=/modoverlay
 booted_bank=$(cat /proc/banktable/booted)
 
+base_file_dir=/tmp/rootfile/emergency
+root_tmp_dir=/tmp/rootfile 
+
+homeware_conversion_tmp=/tmp/homeware_conversion_tmp
+homeware_conversion_dir=/overlay/homeware_conversion
+
+platform_streaming_bank(){
+	return
+}
+
 copy_base_files(){ # <source> <dest>
   preserve_list="/etc/init.d/rootdevice /etc/rc.d/S94rootdevice /usr/sbin/random_seed \
   				/sbin/mount_root-mod /lib/mount_modroot/05_transfer_basefiles \
@@ -12,23 +22,21 @@ copy_base_files(){ # <source> <dest>
 
   for f in $preserve_list; do
     mkdir -p $2$(dirname "$f")
-    cp -a "$2$f" "$3$f"
+    cp -a "$1$f" "$2$f"
   done
 }
 
 # Preserve file from /
 preserve_root() {
-	local root_tmp_dir=/tmp/rootfile
-	local emergencydir=/tmp/rootfile/emergency
 	echo "Copying GUI package and emergency dir to RAM..."
-	mkdir /tmp/rootfile $emergencydir
+	mkdir $root_tmp_dir $base_file_dir
 	if [ -f /root/GUI.tar.bz2 ]; then
 		cp /root/GUI.tar.bz2 $root_tmp_dir/
 	fi
 
-	copy_base_files / $emergencydir
+	copy_base_files "" $base_file_dir
 
-	if [ -f $emergencydir/etc/init.d/rootdevice ]; then
+	if [ -f $base_file_dir/etc/init.d/rootdevice ]; then
 		echo "GUI files preserved!"
 	else
 		echo "GUI files not copied to RAM!"
@@ -36,12 +44,11 @@ preserve_root() {
 }
 
 preserve_config_file() {
-	local config_tmp=/tmp/config_tmp
-	echo "Copying config files to config_tmp dir in RAM..."
-	mkdir /tmp/config_tmp /tmp/shadow_file
-	cp -a $overlay_dir/etc/config/* $config_tmp/
+	echo "Copying config files to homeware_conversion_tmp dir in RAM..."
+	mkdir $homeware_conversion_tmp /tmp/shadow_file
+	cp -a $overlay_dir/etc $homeware_conversion_tmp/
 	cp -a $overlay_dir/etc/shadow /tmp/shadow_file/
-	if [ -f $config_tmp/network ]; then
+	if [ -d $homeware_conversion_tmp/etc ]; then
 		echo "Config files preserved!"
 	else
 		echo "Config file not copied to RAM!"
@@ -64,36 +71,30 @@ root_device() {
 }
 
 restore_config_File() {
-	local config_tmp=/tmp/config_tmp
 
-	if [ -d $config_tmp ]; then
+	if [ -d $homeware_conversion_tmp ]; then
 		echo "Found config dir in RAM!"
-		mkdir -p /overlay/homeware_conversion/etc/config
+		mkdir -p $homeware_conversion_dir
 
-		cp $config_tmp/* /overlay/homeware_conversion/etc/config/
-		cp $config_tmp/modgui /overlay/homeware_conversion/etc/modgui_old
-		cp /tmp/shadow_file/shadow /overlay/homeware_conversion/etc/
+		cp -a $homeware_conversion_tmp/* $homeware_conversion_dir/
 
-		cp /tmp/shadow_file/shadow $overlay_dir/shadow_old
+		mkdir -p $homeware_conversion_dir/etc
+		cp -a $homeware_conversion_tmp/etc/config/modgui $homeware_conversion_dir/etc/modgui_old
+		cp -a /tmp/shadow_file/shadow $overlay_dir/shadow_old
 
 		echo "Config files restored to homeware conversion dir! File will be updated on next boot."
 	fi
 }
 
 restore_base_root() {
-	local emergencydir=/tmp/rootfile/emergency
 	echo "Rooting file not found! Using emergency method."
 	mkdir $overlay_dir
-	cp -a $emergencydir/* $overlay_dir/
+	cp -a $base_file_dir/* $overlay_dir/
 	echo "Device Rooted"
 }
 
 platform_do_upgrade() {
-
-	sleep 10
-	mount_overlay_if_necessary
-
-	local root_tmp_dirt=/tmp/rootfile
+	sleep 10 
 
 	local RESTORE_CONFIG=1
 
@@ -114,7 +115,7 @@ platform_do_upgrade() {
 		overlay_dir=/overlay/$booted_bank
 	fi
 
-	if [ -f $root_tmp_dirt/GUI.tar.bz2 ] || [ -f /etc/init.d/rootdevice ]; then
+	if [ -f $root_tmp_dir/GUI.tar.bz2 ] || [ -f /etc/init.d/rootdevice ]; then
 
 		preserve_root
 
@@ -129,7 +130,7 @@ platform_do_upgrade() {
 
 		# Make sure modoverlay is empty
 		if [ ! "$(ls -A $overlay_dir)" ]; then
-			if [ -f $root_tmp_dirt/GUI.tar.bz2 ]; then
+			if [ -f $root_tmp_dir/GUI.tar.bz2 ]; then
 				root_device
 			else
 				restore_base_root
@@ -140,14 +141,16 @@ platform_do_upgrade() {
 			fi
 
 			if platform_is_dualbank; then
-				if [ -f $overlay_dir/init.d/rootdevice ]; then
+				if [ -f $overlay_dir/etc/init.d/rootdevice ]; then
 					if [ $INSTALL_GUI -eq 1 ]; then
+						mkdir $overlay_dir/root
 						echo 1 > $overlay_dir/root/.install_gui
 					fi
 
 					if mount | grep -q /modoverlay; then
 						# As last step update base in original bank_2 overlay
-	          copy_base_files $overlay_dir /overlay/$booted_bank
+						echo "Update base file in original /overlay"
+						copy_base_files $overlay_dir /overlay/$booted_bank
 					fi
 
 					platform_do_upgrade_bank $1 $booted_bank || exit 1
