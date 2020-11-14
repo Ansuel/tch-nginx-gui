3a02838c1c901dd3452271866b2dffd8f71077c2
Ansuel Smith <ansuelsmth@gmail.com>
2020-11-14 01:15:35 +0100

    Reorganize ci jobs
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- scripts/1-compress_css_javascript.sh
+++ scripts/1-compress_css_javascript.sh
@@ -1,10 +1,10 @@
-TYPE="$(cat $HOME/gui_build/data/type)"
+TYPE="$(cat type)"
 # if [ $TYPE == "DEV" ]; then
 # 	exit 0
 # fi
 
-minify --recursive --verbose --match=\.*.js$ --type=js --output decompressed/ decompressed/
+minify --recursive --verbose --match=\.*.js$ --type=js --output js_files/ js_files/
 
-minify --recursive --verbose --match=\.*.css$ --type=css --output decompressed/ decompressed/
+minify --recursive --verbose --match=\.*.css$ --type=css --output css_files/ css_files/
 
 echo "Finished"
