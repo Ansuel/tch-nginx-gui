9b33c2f0ab25c9b6f423870c53f395a0ee6bc3e0
Ansuel Smith <ansuelsmth@gmail.com>
2020-11-14 01:03:26 +0100

    Fix broken css/js minify for stable build
    Switch to local tool for css/js minify
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- scripts/1-compress_css_javascript.sh
+++ scripts/1-compress_css_javascript.sh
@@ -1,37 +1,10 @@
 TYPE="$(cat $HOME/gui_build/data/type)"
-if [ $TYPE == "DEV" ]; then
-	exit 0
-fi
+# if [ $TYPE == "DEV" ]; then
+# 	exit 0
+# fi
 
-minify_css() {
-	curl -X POST -s --data-urlencode input@"$1" https://cssminifier.com/raw > $1.min
-	chmod $(stat -c "%a" $1) $1.min
-}
+minify --recursive --verbose --match=\.*.js$ --type=js --output decompressed/ decompressed/
 
-minify_js() {
-	curl -X POST -s --data-urlencode input@"$1" https://javascript-minifier.com/raw > $1.min
-	chmod $(stat -c "%a" $1) $1.min
-}
-
-for file in `find . -name "*.css" -type f`; do
-    echo "Minying $file"
-	minify_css "$file" &
-done
-
-for file in `find . -name "*.js" -type f`; do
-    echo "Minying $file"
-	minify_js "$file" &
-done
-
-wait
-
-for file in `find . -name "*.css" -o -name "*.js" -type f`; do
-	if [ -f $file.min ]; then
-		echo "Moving $( echo $file.min | sed 's|.*/||' ) to $( echo $file | sed 's|.*/||' )"
-		rm $file
-		mv $file.min $file
-	fi
-done
-
+minify --recursive --verbose --match=\.*.css$ --type=css --output decompressed/ decompressed/
 
 echo "Finished"
