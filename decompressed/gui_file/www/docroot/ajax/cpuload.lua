83ec27465ebcb8eb42e98cee4a1f24a95ad47cca
Codrut Ban <ban.codrut@gmail.com>
2021-08-04 01:38:50 +0200

    Gui: Display Used memory instead of Free RAM memory (#1045)
    * gui: show used/total RAM instead of free/total (1/2)
    
    Viewing the free memory is useless as that's simply unused portion of memory and it can easily drop under 10MB freaking the user.
    Total-Used=Available , which includes free memory but also includes stuff like Buffered/Cached memory which is dynamically managed to fit the bill.
    
    I opted for showing Used / Total to respect the logic of the surrounding elements: numbers grow as resources consumption grows, but one can prefer Available / Total which is imply subtracting the two as I mentioned previously.
    
    * gui: show used/total RAM instead of free/total (2/2)
    
    * trafficmon: use MemAvailable instead of MemFree
    [ci skip]

--- decompressed/gui_file/www/docroot/ajax/cpuload.lua
+++ decompressed/gui_file/www/docroot/ajax/cpuload.lua
@@ -4,12 +4,12 @@ local readfile = require("web.content_helper").readfile
 local post_helper = require("web.post_helper")
 local ngx = ngx
 
-local ram = tonumber(proxy.get("sys.mem.RAMFree")[1].value or 0) or 0
+local ram = tonumber(proxy.get("sys.mem.RAMUsed")[1].value or 0) or 0
 local cpu_usage = proxy.get("sys.proc.CPUUsage")[1].value or "0"
 
 local data = {
 	cpuusage = cpu_usage .. "%" or "0",
-	ram_free = math.floor(ram / 1024),
+	ram_used = math.floor(ram / 1024),
 	uptime = post_helper.secondsToTime(readfile("/proc/uptime","number",floor)),
 	connection = readfile("/proc/sys/net/netfilter/nf_conntrack_count"),
 	system_time = os.date("%d/%m/%Y %Hh:%Mm:%Ss",os.time()),
